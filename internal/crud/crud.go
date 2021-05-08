934067866d9c9f3f0e680118169aebbcc9c6dc86
Stephen <appcypher@outlook.com>
2021-05-08 14:22:39 +0100

    Round up on error propagation/handling strategy

--- internal/crud/crud.go
+++ internal/crud/crud.go
@@ -4,16 +4,21 @@ import (
 	"context"
 
 	"github.com/gigamono/gigamono-api/internal/graphql/model"
-	"github.com/gigamono/gigamono/pkg/logs"
+	"github.com/gigamono/gigamono/pkg/errs"
+	"github.com/gigamono/gigamono/pkg/messages"
 	"github.com/gigamono/gigamono/pkg/services/auth"
 )
 
 // GetSessionUser get the session user main details.
-func GetSessionUser(ctx context.Context, tokens model.TokensInput) (*model.SessionUser, error) {
+func GetSessionUser(_ context.Context, tokens model.TokensInput) (*model.SessionUser, error) {
 	// TODO: Sec: Validation, Auth, Permission.
 	id, err := auth.GetSessionUserID(auth.Tokens(tokens))
 	if err != nil {
-		return &model.SessionUser{}, logs.NewError("unable to authenticate user", err)
+		panic(errs.NewSystemError(
+			messages.Error["user-auth"].(string),
+			"authenticating user",
+			err,
+		))
 	}
 
 	return &model.SessionUser{
